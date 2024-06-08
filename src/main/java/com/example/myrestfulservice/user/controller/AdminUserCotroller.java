e9645274d06672d8ac95f85e6f4a5f47971410ef
kimjihoon3106 <s24021@gsm.hs.kr>
2024-06-09 01:07:06 +0900

    프로그래밍으로 제어하는 Filtering - 전체 사용자 조회

--- src/main/java/com/example/myrestfulservice/user/controller/AdminUserCotroller.java
+++ src/main/java/com/example/myrestfulservice/user/controller/AdminUserCotroller.java
@@ -11,6 +11,9 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.http.converter.json.MappingJacksonValue;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 @RequestMapping("/admin")
 public class AdminUserCotroller {
@@ -27,6 +30,7 @@ public class AdminUserCotroller {
         User user = service.findOne(id);
 
         AdminUser adminUser = new AdminUser();
+
         if(user == null) {
             throw new UserNotFoundException(String.format("ID[%s] not found", id));
         } else {
@@ -37,6 +41,30 @@ public class AdminUserCotroller {
         FilterProvider filters = new SimpleFilterProvider().addFilter("UserInfo", filter);
 
         MappingJacksonValue mapping = new MappingJacksonValue(adminUser);
+        mapping.setFilters(filters);
+
+        return mapping;
+    }
+
+    // -> /admin/users
+    @GetMapping("/users")
+    public MappingJacksonValue retrieveAllUserAdmin() {
+        List<User> users = service.findAll();
+
+        List<AdminUser> adminUsers = new ArrayList<>();
+        AdminUser adminUser = null;
+
+        for(User user : users) {
+            adminUser = new AdminUser();
+            BeanUtils.copyProperties(user, adminUser);
+
+            adminUsers.add(adminUser);
+        }
+
+        SimpleBeanPropertyFilter filter = SimpleBeanPropertyFilter.filterOutAllExcept("id", "name", "joinDate", "ssn");
+        FilterProvider filters = new SimpleFilterProvider().addFilter("UserInfo", filter);
+
+        MappingJacksonValue mapping = new MappingJacksonValue(adminUsers);
         mapping.setFilters(filters);
 
         return mapping;
